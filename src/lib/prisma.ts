67965ba7d1da0596de950e723d91e0a709e932b0
Michel D <djoumessi.michelc@gmail.com>
2025-08-02 12:46:36 +0200

    Modification du script de build dans package.json pour inclure la génération de Prisma avant le build de Next.js; simplification du fichier prisma.ts en supprimant les commentaires superflus pour une meilleure lisibilité et maintenabilité.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,7 @@
 import { PrismaClient } from ".prisma/client";
 
-// Déclaration d'une instance globale de Prisma pour éviter de créer plusieurs instances en mode développement
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-// En mode développement, on attribue l'instance Prisma à `globalForPrisma.prisma`
-// Cela permet de ne pas créer une nouvelle instance à chaque rechargement du serveur
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
