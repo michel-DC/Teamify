9833e186714a31d61db9ce08fca18197068937b8
Michel D <djoumessi.michelc@gmail.com>
2025-08-16 10:52:09 +0200

    Ajout de balise br dans la route.ts s'occupant de l'envoie d'email, pour plus de cohérence dans le layout

--- src/app/api/events/[slug]/invitations/route.ts
+++ src/app/api/events/[slug]/invitations/route.ts
@@ -175,7 +175,9 @@ export async function POST(
                               <td style="padding:20px;">
                                  <h2 style="font-size:20px; font-weight:600; margin:0 0 15px;">Détails de l'événement</h2>
                                  <p style="margin:0; font-size:15px;"><strong>Événement :</strong> ${eventName}</p>
+                                 <br/>
                                  <p style="margin:0; font-size:15px;"><strong>Catégorie :</strong> ${eventCategory}</p>
+                                 <br/>
                                  ${
                                    eventDate
                                      ? `
@@ -183,6 +185,7 @@ export async function POST(
                                  `
                                      : ""
                                  }
+                                     <br/>
                                  ${
                                    eventLocation
                                      ? `
@@ -190,17 +193,19 @@ export async function POST(
                                  `
                                      : ""
                                  }
+                                 <br/>
                                  <p style="margin:0; font-size:15px;"><strong>Participants :</strong> ${participantsCount} personne${
         participantsCount > 1 ? "s" : ""
       }
                                  </p>
-                                 ${
-                                   truncatedDescription
-                                     ? `
+                                 <br/>
+                                  ${
+                                    truncatedDescription
+                                      ? `
                                  <p style="margin:0; font-size:15px;"><strong>Description :</strong> ${truncatedDescription}</p>
                                  `
-                                     : ""
-                                 }
+                                      : ""
+                                  }
                               </td>
                            </tr>
                         </table>
