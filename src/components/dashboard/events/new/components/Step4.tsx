815db09a5c69cfa7ec74984f26ef7bcc621d3754
Michel D <djoumessi.michelc@gmail.com>
2025-08-17 21:56:15 +0200

    Suppression de l'importation de useState dans le composant Step4 pour alléger le code et améliorer la maintenabilité.

--- src/components/dashboard/events/new/components/Step4.tsx
+++ src/components/dashboard/events/new/components/Step4.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { CloudflareImageUpload } from "@/components/ui/cloudflare-image-upload";
