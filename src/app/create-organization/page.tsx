b058537c207996be14c4482a3cffb0eaa037d4e6
michel-DC <djoumessi.michel08@gmail.com>
2025-09-07 18:13:34 +0200

    Suppression de l'importation de "Metadata" et mise à jour de l'exportation des métadonnées dans le composant de création d'organisation.

--- src/app/create-organization/page.tsx
+++ src/app/create-organization/page.tsx
@@ -1,10 +1,7 @@
-"use client";
-
 import StepWizard from "./components/StepWizard";
 import ClientGate from "./client-gate";
-import { Metadata } from "next";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Teamify - Créer votre première organisation",
   description: "Créer votre première organisation",
 };
