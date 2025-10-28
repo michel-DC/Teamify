38f5d64c40a4744931576e7483acaf8f973b3067
michel-DC <djoumessi.michelc@gmail.com>
2025-10-28 16:37:04 +0100

    Remove sitemap generation file to streamline the application structure and eliminate unused code.

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -16,10 +16,7 @@ export async function GET() {
     (p) => `<url><loc>${BASE_URL}/${p}</loc><changefreq>weekly</changefreq><priority>${p === "" ? "1.0" : "0.7"}</priority></url>`
   ).join("\n");
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${pages}
-</urlset>`;
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${pages}\n</urlset>`;
 
   return new NextResponse(sitemap, {
     headers: {
